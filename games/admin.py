91d62b4318f69aff094b181ca514e39a3c4a4409
Rotem Cohen <rotembcohen@gmail.com>
2017-08-08 13:51:11 -0400

    models admin registration

--- games/admin.py
+++ games/admin.py
@@ -2,5 +2,7 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from games.models import Game, Bet
 
-# Register your models here.
+admin.site.register(Game)
+admin.site.register(Bet)
--- users/admin.py
+++ users/admin.py
@@ -2,5 +2,7 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from users.models import User
 
-# Register your models here.
+admin.site.register(User)
+
