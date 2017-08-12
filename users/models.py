97e5d7c518f529d99173a89a7841b9517bdda4cd
Rotem Cohen <rotem@timeflash.com>
2017-08-12 18:24:22 -0400

    Revert "Revert "FB integration user model changes""
    This reverts commit 3d761a60613ab4ed8ee1676ca0322fd03e0fbc02.

--- users/models.py
+++ users/models.py
@@ -12,6 +12,8 @@ from rest_framework.authtoken.models import Token
 class User(AbstractUser):
 	venmo_username = models.CharField(max_length=255, blank=True, null=True)
 	phone_number = models.CharField(max_length=255, blank=True, null=True)
+	facebook_token = models.CharField(max_length=255, blank=True, null=True)
+	profile_url = models.CharField(max_length=255, blank=True, null=True)
 
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
 def create_auth_token(sender, instance=None, created=False, **kwargs):
