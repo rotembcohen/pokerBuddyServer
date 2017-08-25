3772ae16c8dec3c759a5c0bd1fa8a323d535efa7
Rotem Cohen <rotembcohen@gmail.com>
2017-08-25 15:58:58 -0400

    removal of constants

--- pokerBuddyServer/settings.py
+++ pokerBuddyServer/settings.py
@@ -19,12 +19,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ')&l8^&!$rtke-^-7j0p&e-_z%-vl53@!!5ps-9a+uddd_&p=0y'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
 ALLOWED_HOSTS = ['*']
 
 
